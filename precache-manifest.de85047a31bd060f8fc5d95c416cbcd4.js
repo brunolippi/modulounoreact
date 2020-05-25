e2b6641b49ab3fe8951b11279bc3541971db7584
bzlippi <63300215+bzlippi@users.noreply.github.com>
2020-05-25 03:34:43 -0300

    Updates

---
+++ precache-manifest.de85047a31bd060f8fc5d95c416cbcd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e26a47c47ddcd29cbfc6740f039685",
+    "url": "/modulounoreact/index.html"
+  },
+  {
+    "revision": "a4d758ea880b7d0a664a",
+    "url": "/modulounoreact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "886e60f29a4f36e273cf",
+    "url": "/modulounoreact/static/css/main.d36df6ae.chunk.css"
+  },
+  {
+    "revision": "a4d758ea880b7d0a664a",
+    "url": "/modulounoreact/static/js/2.aa0182cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/modulounoreact/static/js/2.aa0182cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886e60f29a4f36e273cf",
+    "url": "/modulounoreact/static/js/main.47bc2dd7.chunk.js"
+  },
+  {
+    "revision": "0d70b83ccfc5d836824c",
+    "url": "/modulounoreact/static/js/runtime-main.4278cb9e.js"
+  }
+]);
